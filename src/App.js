c491ca3800ae8e4ba7456aca803f6bc16047698e
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-10-13 15:43:53 +0100

    task upD/delete/cancel/done updated

--- src/App.js
+++ src/App.js
@@ -35,23 +35,49 @@ function App() {
 
   // Add Tasks
 
-  const addTask = () => {};
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask('');
+    }
+  };
 
   // Delete Tasks
 
-  const deleteTask = (id) => {};
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter((task) => task.id !== id);
+    setToDo(newTasks);
+  };
 
   //  Completed Task marked as done
 
-  const doneTask = (id) => {};
+  const doneTask = (id) => {
+    let newTask = toDo.map((task) => {
+      if (task.id === id) {
+        return { ...task, status: !task.status };
+      }
+      return task;
+    });
+    setToDo(newTask);
+  };
 
   // cancel marked as done
 
-  const cancelDoneTask = () => {};
+  const cancelDoneTask = () => {
+    setUpdateData('');
+  };
 
   // Change Task
 
-  const changeTask = (e) => {};
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title: updateData.title,
+      status: updateData.status ? true : false,
+    };
+  };
 
   // Update Task
 
@@ -63,9 +89,37 @@ function App() {
       <h1>React Built ToDo List</h1>
       <br />
 
+      {/* Update Task */}
+
       <div className="row">
         <div className="col">
-          <input className="form-control form-control-lg" />
+          <input
+            value={updateData && updateData.title}
+            onChange={(e) => changeTask(e)}
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success mr-20">Update</button>
+          <button className="btn btn-lg btn-warning">Cancel</button>
+        </div>
+      </div>
+      <br />
+
+      {/* Add Tasks  */}
+
+      <div className="row">
+        <div className="col mb-3">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-auto">
+          <button onClick={addTask} className="btn btn-lg btn-success">
+            Add Task
+          </button>
         </div>
       </div>
       {/* Display ToDo's Section */}
@@ -84,13 +138,27 @@ function App() {
                     <span className="taskText">{task.title}</span>
                   </div>
                   <div className="iconsWrap">
-                    <span title="Completed / Not Completed">
+                    <span
+                      title="Completed / Not Completed"
+                      onClick={(e) => doneTask(task.id)}
+                    >
                       <FontAwesomeIcon icon={faCircleCheck} />
                     </span>
-                    <span title="Edit">
-                      <FontAwesomeIcon icon={faPen} />
-                    </span>
-                    <span title="Delete">
+                    {task.status ? null : (
+                      <span
+                        onClick={(e) =>
+                          setUpdateData({
+                            id: task.id,
+                            tite: task.title,
+                            status: task.status ? true : false,
+                          })
+                        }
+                        title="Edit"
+                      >
+                        <FontAwesomeIcon icon={faPen} />
+                      </span>
+                    )}
+                    <span onClick={() => deleteTask(task.id)} title="Delete">
                       <FontAwesomeIcon icon={faTrashCan} />
                     </span>
                   </div>
